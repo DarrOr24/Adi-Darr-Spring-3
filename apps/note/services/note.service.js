c407622cd910666f0a367a385e34e4b596113cad
Darr Or <darrmorgan@gmail.com>
2024-05-21 11:05:37 -0400

    Create basic noteService with demo data

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -7,9 +7,27 @@ _createNotes()
 
 export const noteService = {
     query,
-    get,
-    remove,
-    save,
+    // get,
+    // remove,
+    // save,
+}
+
+window.ns = noteService
+
+function query(filterBy = {}) {
+    return storageService.query(NOTE_KEY)
+        .then(notes => {
+            if (filterBy.txt) {
+                const regExp = new RegExp(filterBy.txt, 'i')
+                notes = notes.filter(note => regExp.test(note.info.title))
+            }
+
+            // if (filterBy.minSpeed) {
+            //     notes = notes.filter(note => note.maxSpeed >= filterBy.minSpeed)
+            // }
+
+            return notes
+        })
 }
 
 function _createNotes() {
@@ -19,14 +37,14 @@ function _createNotes() {
         const noteTitles = ['Note1', 'Note2', 'Note3', 'Note4', 'Note5']
         for (let i = 0; i < 5; i++){
             const noteTitle = noteTitles[i]
-            notes.push(_createNote('NoteTxt', false, {'backgroundColor': utilService.getRandomColor()}, info ={'title': noteTitle, 'txt': utilService.makeLorem(20)}))
+            notes.push(_createNote('NoteTxt', false, {'backgroundColor': utilService.getRandomColor()}, {'title': noteTitle, 'txt': utilService.makeLorem(20)}))
         }
         utilService.saveToStorage(NOTE_KEY, notes)
     }
 }
 
 
-function _createNote(type='NoteTxt', isPinned=false, style, info={}){
+function _createNote(type, isPinned, style, info){
     const note = {
                     id:  utilService.makeId(),        
                     type, 

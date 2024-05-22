a03247e526b5d44fb0a147c93351ee607164fb42
Darr Or <darrmorgan@gmail.com>
2024-05-22 10:00:56 -0400

    Add actions icons and hovering features to notes but not functional yet

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -8,7 +8,15 @@ export function NoteList({ notes, onRemove }) {
             <li key={note.id } style={{backgroundColor: note.style.backgroundColor}}>
                 <NotePreview note={note} />
                 <section className ="action-icons">
-                    <button onClick={() => onRemove(note.id)}>x</button>
+                    <div><img src="assets\img\check.svg" alt="" /></div>
+                    <div><img src="assets\img\pin.svg" alt="" /></div>
+                    <div><img src="assets\img\remind_me.svg" alt="" /></div>
+                    <div><img src="assets\img\collaborator.svg" alt="" /></div>
+                    <div><img src="assets\img\background_options.svg" alt="" /></div>
+                    <div><img src="assets\img\add_image.svg" alt="" /></div>
+                    <div><img src="assets\img\archive.svg" alt="" /></div>
+                    <div><img src="assets\img\more.svg" alt="" /></div>
+                    {/* <button onClick={() => onRemove(note.id)}>x</button> */}
                 </section>
             </li>)}
         </ul>
--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -7,7 +7,7 @@ export function NotePreview({ note }){
         <article className="note-preview" >
             <header>
                 <h2>{title}</h2>
-                <img src="\assets\img\pin.svg" alt="" />
+                
             </header>
             
             <p>{txt} km/h</p>

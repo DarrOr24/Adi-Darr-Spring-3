e0b8f3458431ad2e9a3a96a09135b2ba43d5774b
Darr Or <darrmorgan@gmail.com>
2024-05-24 14:16:55 -0400

    Change edit display to be called from note preview and to hide the note from the list when displayed

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -1,29 +1,47 @@
+const { useState } = React
+const { useNavigate } = ReactRouter
+
 import { ActionBtns } from "./ActionBtns.jsx"
+import { NoteEdit2 } from "./NoteEdit2.jsx"
 import { NotePin } from "./NotePin.jsx"
 
-const { useState } = React
-
-export function NotePreview({ note, onRemove}){
+export function NotePreview({ note, onRemove, onEdit}){
     const { info, style } = note
     const { title, txt } = info
     const { backgroundColor } = style
 
-
-    const [ openNote, setOpenNote ] = useState('')
-
-    function openEdit(){
-        setOpenNote('open')
-    }
+    const navigate = useNavigate()
+    const [ openNote, setOpenNote ] = useState(false)
     
-    return (
-        <article onClick = {openEdit} className={`note-preview ${openNote}`} >
-            <h2>{title}</h2>
-            <p >{txt}</p>
-            <NotePin />
-            <ActionBtns note={note} onRemove={onRemove} />
-            
-        </article>
-    ) 
+
+    function openEdit(){  
+        setOpenNote(true)
+        navigate(`/note/edit/${note.id}`) 
+    }
+
+    function closeNoteEdit(){ 
+        setOpenNote(false)
+        navigate('/note')
+        
+    }
+
+    
+
+    return  <section>
+
+                {!openNote && 
+                    <article onClick = {openEdit} className="note-preview" style={{backgroundColor: note.style.backgroundColor}} >
+                        <h2>{title}</h2>
+                        <p >{txt}</p>
+                        <NotePin />
+                        <ActionBtns note={note} onRemove={onRemove} />
+                    </article>}
+
+                {openNote && <NoteEdit2 noteToEdit = {note} onClose={closeNoteEdit} onEdit={onEdit}/>}
+                
+    </section>
+    
+    
 }
 
 

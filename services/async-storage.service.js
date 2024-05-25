a2799a6708878dd32827eed7e919c6049925a18e
Darr Or <darrmorgan@gmail.com>
2024-05-25 18:27:41 -0400

    Add postFirst function to storage

--- services/async-storage.service.js
+++ services/async-storage.service.js
@@ -2,6 +2,7 @@ export const storageService = {
     query,
     get,
     post,
+    postFirst,
     put,
     remove,
 }
@@ -24,6 +25,16 @@ function post(entityType, newEntity) {
     newEntity.id = _makeId()
     return query(entityType).then(entities => {
         entities.push(newEntity)
+        _save(entityType, entities)
+        return newEntity
+    })
+}
+
+function postFirst(entityType, newEntity) {
+    newEntity = {...newEntity}
+    // newEntity.id = _makeId()
+    return query(entityType).then(entities => {
+        entities.unshift(newEntity)
         _save(entityType, entities)
         return newEntity
     })

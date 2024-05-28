a44a28eb78ff3fd0b5395bb0c48fb71484a77fa3
AdiSabban10 <adialon050@gmail.com>
2024-05-28 20:33:46 +0300

    Fix email list css

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -2,6 +2,7 @@ import { utilService } from '../../../services/util.service.js'
 
 export function MailPreview({ mail }) {
     const sentDate = new Date(mail.sentAt)
+    const currentDate = new Date()
     const day = sentDate.getDate()
     const monthName =  utilService.getMonthName(sentDate)
 
@@ -9,13 +10,23 @@ export function MailPreview({ mail }) {
     const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: true }
     const formattedTime = sentDate.toLocaleTimeString('en-IL', timeOptions).toLocaleUpperCase()
     
+    const timeDifference = currentDate - sentDate
+
+    const oneDayInMilliseconds = 24 * 60 * 60 * 1000
+    const hasDayPassed = timeDifference >= oneDayInMilliseconds
+
     const { from, subject, body, isRead } = mail
     return (
         <article className="mail-preview">
-            <h3>{from}</h3>
-            <h3>{subject}</h3>
-            <p>{body}</p>
-            <p>{monthName} {day} {formattedTime}</p>
+            <div className="from">{from}</div>
+            <div className="txt">
+                <p className="subject">{subject}</p>
+                <p className="body">{body}</p>    
+            </div>
+            <div className="time">
+                {hasDayPassed ? `${monthName} ${day}` : formattedTime}
+            </div>
+            {/* <p>{monthName} {day} {formattedTime}</p> */}
         </article>
     )
 }

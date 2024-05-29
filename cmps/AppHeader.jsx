c1ffb5439b687e79533201605ca8e291acf7354a
Darr Or <darrmorgan@gmail.com>
2024-05-29 15:20:29 -0400

    Finish nav icon app header css

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -5,17 +5,23 @@ const { Link, NavLink } = ReactRouterDOM
 export function AppHeader() {
 
     const [showMenu, setShowMenu] = useState(false)
+    const [iconClass, setIconClass] = useState('')
+    
 
 
     function openHeaderNav(){
-        setShowMenu(prevShowMenu => !prevShowMenu)    
+        setShowMenu(prevShowMenu => !prevShowMenu)
+        if (!iconClass) setIconClass('clicked')
+        else setIconClass('')
+         
+
     }
 
 
     return <header className="app-header">
        
        <div>
-            <div onClick={openHeaderNav} className={`action-icon main-nav-icon `}>
+            <div onClick={openHeaderNav} className={`action-icon main-nav-icon  ${iconClass}`} >
                         <img height="30" src="assets\img\dots-nav.png" alt="" />
                         <span className="action-name select">DARRADI APPS</span>  
                         {(showMenu) &&  <nav>
